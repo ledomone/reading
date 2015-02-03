2668c0609e881f009702237075cadeb8159319a5
Dominik <ledom.net+github@gmail.com>
2015-02-03 23:27:57 +0100

    fixed bug#1 added UserCreationForm

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class MyUser(AbstractUser):
-    pass
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+
