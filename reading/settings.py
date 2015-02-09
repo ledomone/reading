3f45665d3a70aab7264bde69a92d87d19a7d9850
Dominik <ledom.net+github@gmail.com>
2015-02-09 23:59:40 +0100

    class configurations

--- reading/settings.py
+++ reading/settings.py
@@ -8,125 +8,136 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+from configurations import Configuration
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+class Production(Configuration):
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%_jqa9^-16i&v1x^6=p)$=dy#h&!w95*2ozmz5a%3ehkx4jz#g'
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = '%_jqa9^-16i&v1x^6=p)$=dy#h&!w95*2ozmz5a%3ehkx4jz#g'
 
-TEMPLATE_DEBUG = True
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+    TEMPLATE_DEBUG = False
 
-
-# Application definition
-
-INSTALLED_APPS = (
-    'users',
-    'logbook',
-    #############################
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    #############################
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'reading.urls'
-
-WSGI_APPLICATION = 'reading.wsgi.application'
+    ALLOWED_HOSTS = []
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+    # Application definition
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    INSTALLED_APPS = (
+        'users',
+        'logbook',
+        #############################
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        #############################
+
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.sites',
+    )
+
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ROOT_URLCONF = 'reading.urls'
+
+    WSGI_APPLICATION = 'reading.wsgi.application'
+
+
+    # Database
+    # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+    TIME_ZONE = 'UTC'
 
-USE_I18N = True
+    USE_I18N = True
 
-USE_L10N = True
+    USE_L10N = True
 
-USE_TZ = True
+    USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+    STATIC_URL = '/static/'
 
-AUTH_USER_MODEL = 'users.MyUser'
+    AUTH_USER_MODEL = 'users.MyUser'
 
-TEMPLATE_DIRS = {
-    os.path.join(BASE_DIR, 'templates'),
-}
+    TEMPLATE_DIRS = {
+        os.path.join(BASE_DIR, 'templates'),
+    }
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages",
+    TEMPLATE_CONTEXT_PROCESSORS = (
+        "django.contrib.auth.context_processors.auth",
+        "django.core.context_processors.debug",
+        "django.core.context_processors.i18n",
+        "django.core.context_processors.media",
+        "django.core.context_processors.static",
+        "django.core.context_processors.tz",
+        "django.contrib.messages.context_processors.messages",
 
-    # Required by allauth template tags
-    "django.core.context_processors.request",
-    # allauth specific context processors
-    "allauth.account.context_processors.account",
-    "allauth.socialaccount.context_processors.socialaccount",
-)
+        # Required by allauth template tags
+        "django.core.context_processors.request",
+        # allauth specific context processors
+        "allauth.account.context_processors.account",
+        "allauth.socialaccount.context_processors.socialaccount",
+    )
 
-AUTHENTICATION_BACKENDS = (
+    AUTHENTICATION_BACKENDS = (
 
-    # Needed to login by username in Django admin, regardless of `allauth`
-    "django.contrib.auth.backends.ModelBackend",
+        # Needed to login by username in Django admin, regardless of `allauth`
+        "django.contrib.auth.backends.ModelBackend",
 
-    # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
-)
+        # `allauth` specific authentication methods, such as login by e-mail
+        "allauth.account.auth_backends.AuthenticationBackend",
+    )
 
-SITE_ID = 1
+    SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    LOGIN_URL = "main-page"
 
-LOGIN_URL = "main-page"
+    LOGIN_REDIRECT_URL = "main-page"
 
-LOGIN_REDIRECT_URL = "main-page"
+
+class Dev(Production):
+    DEBUG = True
+
+    TEMPLATE_DEBUG = True
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
