8d620048749a3b2a3111cc86eb484e8c18a8dffa
Dominik <ledom.net+github@gmail.com>
2015-02-11 23:08:29 +0100

    added book_detail view

--- logbook/views.py
+++ logbook/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
+
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Book
-
-
-# Create your views here.
-from django.views.generic import TemplateView, ListView
 
 
 class MainPageView(TemplateView):
@@ -19,3 +17,8 @@ class BookListView(ListView):
     def get_queryset(self):
         qs = super(BookListView, self).get_queryset()
         return qs.filter(who=self.request.user.id)
+
+
+class BookView(DetailView):
+    model = Book
+    template_name = 'logbook/book_detail.html'
--- reading/urls.py
+++ reading/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from logbook.views import BookListView
+from logbook.views import BookListView, BookView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^books/$', BookListView.as_view(), name='book-list'),
+    url(r'^books/(?P<pk>\d+)/$', BookView.as_view(), name='book-detail'),
     url(r'^$', 'logbook.views.index_view', name='main-page'),
     url(r'^accounts/', include('allauth.urls')),
 )
