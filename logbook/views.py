692a40d06ce036d81f0439287ac0449e0e1cb26b
Dominik <ledom.net+github@gmail.com>
2015-02-28 14:15:34 +0100

    login required for all views

--- logbook/views.py
+++ logbook/views.py
@@ -29,12 +29,19 @@ class BookView(DetailView):
     model = Book
     template_name = 'logbook/book_detail.html'
 
+    def get_queryset(self):
+        qs = super(BookView, self).get_queryset()
+        return qs.filter(who=self.request.user)
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(BookView, self).dispatch(request, args, kwargs)
+
 
 class BookCreate(CreateView):
     model = Book
     template_name_suffix = '_create_form'
     fields = ['title', 'author', 'pages', 'date']
-    # success_url = '/books'
 
     def get_success_url(self):
         return reverse('logbook:book-list')
@@ -43,7 +50,19 @@ class BookCreate(CreateView):
         form.instance.who = self.request.user
         return super(BookCreate, self).form_valid(form)
 
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(BookCreate, self).dispatch(request, args, kwargs)
+
 
 class BookDelete(DeleteView):
     model = Book
     success_url = reverse_lazy('logbook:book-list')
+
+    def get_queryset(self):
+        qs = super(BookDelete, self).get_queryset()
+        return qs.filter(who=self.request.user)
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(BookDelete, self).dispatch(request, args, kwargs)
