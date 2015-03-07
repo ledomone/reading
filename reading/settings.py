7e36df7149aded757a8516f0ba4bf5fa2271ac34
Dominik <ledom.net+github@gmail.com>
2015-03-07 17:01:16 +0100

    second template

--- reading/settings.py
+++ reading/settings.py
@@ -96,7 +96,12 @@ class Production(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
     STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "static"),
+    )
 
     AUTH_USER_MODEL = 'users.MyUser'
 
